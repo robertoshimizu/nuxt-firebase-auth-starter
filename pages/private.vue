5c5b08d535707e9fe4dc9d4d8d35ea9b08075f1f
Roberto Shimizu <rshimizubr@gmail.com>
2022-03-30 11:38:33 -0300

    fix: :bug: neutralize middleware.global

--- pages/private.vue
+++ pages/private.vue
@@ -13,6 +13,7 @@ const signOut = async() => {
 </script>
 
 <template>
+  Private
   <div v-if="user">
     <div>Welcome, {{ user.email }}</div>
     <div>{{ data ? data.message : 'Fetching...' }}</div>
--- plugins/middleware.global.ts
+++ plugins/middleware.global.ts
@@ -1,14 +1,17 @@
 export default defineNuxtPlugin(() => {
-  const user = useUser()
-  addRouteMiddleware(
-    'auth',
-    (to) => {
-      if (to.path === '/' && user.value)
-        return navigateTo('/private')
+  // const user = useUser()
+  // addRouteMiddleware(
+  //   'auth',
+  //   (to) => {
+  //     if (to.path === '/' && user.value)
+  //       return navigateTo('/private')
 
-      if (to.path !== '/' && !user.value)
-        return navigateTo('/')
-    },
-    { global: true },
-  )
+  //     if (to.path !== '/' && !user.value)
+  //       return navigateTo('/')
+
+  //     if (to.path === '/login')
+  //       return navigateTo('/login')
+  //   },
+  //   { global: true },
+  // )
 })
