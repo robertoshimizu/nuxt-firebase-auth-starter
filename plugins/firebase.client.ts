b57774fa49dc5da78ab657837987f22c8d8bc34f
Roberto Shimizu <rshimizubr@gmail.com>
2022-03-30 11:04:01 -0300

    feat: :mag: add favicon

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -9,14 +9,19 @@ export default defineNuxtPlugin(() => {
     apiKey: config.firebaseApiKey,
     authDomain: config.firebaseAuthDomain,
   }
+  try {
+    const app = initializeApp(firebaseConfig)
+    const auth = getAuth(app)
 
-  const app = initializeApp(firebaseConfig)
-  const auth = getAuth(app)
-
-  return {
-    provide: {
-      firebaseApp: app,
-      firebaseAuth: auth,
-    },
+    return {
+      provide: {
+        firebaseApp: app,
+        firebaseAuth: auth,
+      },
+    }
+  }
+  catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(`Firebase failed to be initialized: ${error}`)
   }
 })
