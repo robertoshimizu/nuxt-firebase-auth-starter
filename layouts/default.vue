89609e988287b40762a9448a56f52df1902673a5
Roberto Shimizu <rshimizubr@gmail.com>
2022-03-30 22:58:50 -0300

    fix: :ambulance: add div at root component

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,28 +6,30 @@ userAuthChange()
 </script>
 
 <template>
-  <nav style="display:flex;justify-content: start;">
-    <a style="align-self:center" href="/">
-      <img style="height: 40px;" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="logo">
-    </a>
-    <ul style="list-style-type: none;flex:1;display:flex;align-items: flex-start;justify-content: flex-start;">
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/login">Login</a>
-      </li>
-      <li>
-        <a href="/signup">Signup</a>
-      </li>
-    </ul>
-  </nav>
-
   <div>
-    <slot />
-  </div>
+    <nav style="display:flex;justify-content: start;">
+      <a style="align-self:center" href="/">
+        <img style="height: 40px;" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="logo">
+      </a>
+      <ul style="list-style-type: none;flex:1;display:flex;align-items: flex-start;justify-content: flex-start;">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+        <li>
+          <a href="/signup">Signup</a>
+        </li>
+      </ul>
+    </nav>
 
-  <footer>Footer</footer>
+    <div>
+      <slot />
+    </div>
+
+    <footer>Footer</footer>
+  </div>
 </template>
 <style>
 a:link {
